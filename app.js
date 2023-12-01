1ed7d42dfce057e59963aaafb07321e0abf69eca
Admin <>
2023-11-30 21:39:59 -0800

    basicAPIs

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 import express from 'express'
 import {} from 'dotenv/config'
+import routes from './routes/routes.js'
 
-const router = express.Router()
 const app = express()
 
-app.use(express.static('public'))
+//LOAD ROUTES INTO OUR MAIN FILE 
 
-// router.get('/', (req, res) => {
-//   res.sendFile('index.html', {root: 'public'})
-// })
+app.use('/api/v1/employees', routes)
 
-// router.get('/about', (req, res) => {
-//   res.sendFile('about.html', {root: 'public'})
-// })
-
-router.route('/').get((req, res) => res.sendFile('index.html', {root: 'public'}))
-router.route('/about').get((req, res) => res.sendFile('about.html', {root: 'public'}))
-
-app.use(router)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
